e676df18296744def622b21722ba9e400ecdcd63
Capn05 <89543708+Capn05@users.noreply.github.com>
2024-10-13 02:05:05 -0400

    portfolio page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home"; // Assuming Home is in the correct path
-import Environmental from "./pages/Environmental";
+import Environmental from "./pages/environmental";
 import Social from "./pages/Social";
 import Governance from "./pages/Governance";
+import Portfolio from "./pages/portfolio";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/environmental" element={<Environmental />} />
         <Route path="/social" element={<Social />} />
         <Route path="/governance" element={<Governance />} />
+        <Route path = '/portfolio' element={<Portfolio/>}></Route>
       </Routes>
     </Router>
   );

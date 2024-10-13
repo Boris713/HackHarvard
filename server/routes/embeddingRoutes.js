3b687c1c4d7f6bcb54cd8b3c51a3b725ff874ef6
Nico Fidalgo <nicom.fidalgo@gmail.com>
2024-10-13 02:48:21 -0400

    Added more API endpoints

--- server/routes/embeddingRoutes.js
+++ server/routes/embeddingRoutes.js
@@ -2,18 +2,18 @@ import express from 'express';
 import { generateTextEmbedding } from '../controllers/embeddingController.js';
 import { semanticSearch } from '../controllers/semanticSearchController.js';
 import { estimateSustainabilityScore } from '../controllers/companyEstimateController.js';
-import { getAllCompanies, getCompanyByName } from '../controllers/getCompanyInfoController.js';
-import { getCompaniesByUser } from '../controllers/getCompanyInfoController.js';
-import { addCompanyToUser } from '../controllers/getCompanyInfoController.js';
+import { getAllCompanies, getCompanyByName, getCompaniesByUser, addCompanyToUser, deleteCompanyFromUser, updateUserScore } from '../controllers/getCompanyInfoController.js';
 
 const router = express.Router();
 
 router.post('/embedding', generateTextEmbedding);
-router.post('/semanticSearch',semanticSearch);
+router.post('/semanticSearch', semanticSearch);
 router.post('/estimateScore', estimateSustainabilityScore);
 router.get('/companies', getAllCompanies);
 router.get('/company/name/:name', getCompanyByName);
 router.get('/user/:ID/companies', getCompaniesByUser);
 router.post('/user/addCompany', addCompanyToUser);
+router.post('/user/deleteCompany', deleteCompanyFromUser);
+router.post('/user/updateScore', updateUserScore);
 
 export default router;
